2f0a41b16052e05e2ac5181a3647c4249687fff8
Benergy Meenan Ravuri <quic_bravuri@quicinc.com>
2023-06-29 22:00:04 +0530

    Revert "sun: Disable QESDK temporarily"
    This reverts commit e22384a502fb2c0e007aa70bba5ad1cf70dfa4c1.
    
    Change-Id: Iecee105e120d8b9720d83626785003d7401a2ee0

--- sun.mk
+++ sun.mk
@@ -511,12 +511,10 @@ ifeq ($(TARGET_USES_QMAA), true)
 ifneq ($(TARGET_USES_QMAA_OVERRIDE_ANDROID_CORE),true)
 PRODUCT_ENABLE_QESDK := false
 else
-# Disable QESDK temporarily for QESDK component creation
-PRODUCT_ENABLE_QESDK := false
+PRODUCT_ENABLE_QESDK := true
 endif
 else
-# Disable QESDK temporarily for QESDK component creation
-PRODUCT_ENABLE_QESDK := false
+PRODUCT_ENABLE_QESDK := true
 endif
 
 # Vendor property to enable advanced network scanning
