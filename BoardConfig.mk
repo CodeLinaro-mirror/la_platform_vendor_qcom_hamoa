fc5a0c789128177264708f5407d2a17cdf73afa4
Bruce Levy <quic_blevy@quicinc.com>
2024-05-30 15:33:27 -0700

    canoe: Modify security profile xml filename
    Change canoe security profile xml filename. Remove "tz" from the
    filename.
    
    Change-Id: Ib587f35308704f8662153ec5724dcb7d78461e9b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,7 @@ BOARD_RAMDISK_USE_LZ4 := true
 
 -include $(QCPATH)/common/canoe/BoardConfigVendor.mk
 
-SECTOOLS_SECURITY_PROFILE := $(QCPATH)/securemsm/security_profiles/canoe_tz_security_profile.xml
+SECTOOLS_SECURITY_PROFILE := $(QCPATH)/securemsm/security_profiles/canoe_security_profile.xml
 
 USE_OPENGL_RENDERER := true
 
