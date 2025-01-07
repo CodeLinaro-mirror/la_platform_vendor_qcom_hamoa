4ca029c1565534e58350df44795e5c3aad9a71ed
Subbaraman Narayanamurthy <quic_subbaram@quicinc.com>
2025-01-06 16:33:59 -0800

    init.target.rc: Remove mounting modem partition for charger mode
    On targets like Canoe, charger FW is running on SOCCP subsystem
    which is bought out of reset earlier in the early bootloader
    stage. Hence, modem partition need not have to be mounted in
    Linux HLOS to support charger mode. Remove it.
    
    Change-Id: I4021a6cee85592d09675e2f5a203c2551dd957f6

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -72,18 +72,6 @@ endif
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-ifeq ($(ENABLE_AB), true)
-    LOCAL_MODULE       := charger_fw_fstab.qti
-else
-    LOCAL_MODULE       := charger_fw_fstab_non_AB.qti
-endif
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := charger_fw_fstab.qti
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
-
 include device/qcom/vendor-common/MergeConfig.mk
 
 #----------------------------------------------------------------------
--- canoe.mk
+++ canoe.mk
@@ -507,13 +507,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
 
-#Charger
-ifeq ($(ENABLE_AB),true)
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/charger_fw_fstab.qti:$(TARGET_COPY_OUT_VENDOR)/etc/charger_fw_fstab.qti
-else
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/charger_fw_fstab_non_AB.qti:$(TARGET_COPY_OUT_VENDOR)/etc/charger_fw_fstab.qti
-endif
-
 PRODUCT_BOOT_JARS += tcmiface
 PRODUCT_BOOT_JARS += telephony-ext
 PRODUCT_PACKAGES += telephony-ext
