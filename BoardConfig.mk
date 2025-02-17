7e99c839a57e46696a0f86d9a4b008fda50987b0
Mohit Sharma <quic_smohit@quicinc.com>
2025-02-17 15:00:36 +0530

    canoe: Add security profile path for Alor.
    Added security profile path for Alor.
    
    Change-Id: I39de6e1aee2e2efcdfb80b6366641d42339c2628

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@ BOARD_RAMDISK_USE_LZ4 := true
 
 -include $(QCPATH)/common/canoe/BoardConfigVendor.mk
 
-SECTOOLS_SECURITY_PROFILE := $(QCPATH)/securemsm/security_profiles/canoe_security_profile.xml
+SECTOOLS_SECURITY_PROFILE := $(QCPATH)/securemsm/security_profiles/canoe_security_profile.xml $(QCPATH)/securemsm/security_profiles/alor_security_profile.xml
 
 USE_OPENGL_RENDERER := true
 
