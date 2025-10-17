6f830e6dd1deab34161fdc7f169c7ff0008c99eb
suresh dangatla <sdangatl@qti.qualcomm.com>
2025-10-17 16:10:22 +0530

    Remove duplicate compatibility matrix file configuration
    Change-Id: I5be3ba6ec7234c55ab84822a195a12118174f3aa

--- hamoa.mk
+++ hamoa.mk
@@ -18,9 +18,6 @@ BOARD_AVB_ENABLE := true
 
 $(call inherit-product, device/qcom/hamoa/common64.mk)
 
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml \
-
 TARGET_USES_AL := true
 
 TARGET_FWK_SUPPORTS_FULL_VALUEADDS := false
